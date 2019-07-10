5c4cc033ae240d9f91981c358ff128c378e2c9bf
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-07-10 10:46:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace urban_planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building myHouse = new Building("2113 Old Greenbrier Pike"){
+                Width = 500,
+                Depth = 500,
+                Stories = 27,
+            };
+            myHouse.Construct();
+            myHouse.Purchase("Addam Joor");
+            myHouse.Info();
+        }
+    }
+}
