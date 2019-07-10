d3a1b7f1e62e4b6d7b6e704cd03d0960e68c1ee8
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-07-10 10:59:05 -0500

    corrected fields

--- Building.cs
+++ Building.cs
@@ -4,10 +4,10 @@ namespace urban_planner
 {
     public class Building
     {
-        private string _designer { get; set; }
-        private DateTime _dateConstructed { get; set; }
-        private string _address { get; set; }
-        private string _owner { get; set; }
+        private string _designer = "Addam Joor";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
         public int Stories { get; set; }
         public double Width { get; set; }
         public double Depth { get; set; }
@@ -37,6 +37,7 @@ namespace urban_planner
             Console.WriteLine("");
             Console.WriteLine(_address);
             Console.WriteLine("--------------------");
+            Console.WriteLine($"Designed by {_designer}");
             Console.WriteLine($"Constructed on {_dateConstructed}");
             Console.WriteLine($"Owned by {_owner}");
             Console.WriteLine($"{Volume} cubic meters of space");
